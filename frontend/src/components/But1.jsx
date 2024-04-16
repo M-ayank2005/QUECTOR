f303f59e8334d701c9989b527e8f04d8c3852ddc
rishyym0927 <rishyym0927@gmail.com>
2024-04-16 22:05:08 +0530

    login

---
+++ frontend/src/components/But1.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function But1({onClick}) {
+  return (
+    
+    <button onClick={onclick} className="btn btn-wide bg-black  text-white hover:bg-black hover:text-white w-full">Log in</button>
+    
+  )
+}
