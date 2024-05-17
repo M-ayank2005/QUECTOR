d02c82393c162f30361f5ba8bf9d661827051edc
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2024-05-17 16:47:49 +0530

    added_emailService

---
+++ backend_db/src/main/java/net/javaguides/springboot/controller/EmailController.java
@@ -0,0 +1,46 @@
+package net.javaguides.springboot.controller;
+
+// Java Program to Create Rest Controller that
+// Defines various API for Sending Mail
+
+
+
+// Importing required classes
+import net.javaguides.springboot.entity.EmailDetails;
+import  net.javaguides.springboot.security.services.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Annotation
+@RestController
+@RequestMapping("/api")
+public class EmailController {
+
+	@Autowired private EmailService emailService;
+
+	// Sending a simple Email
+	@PostMapping("/sendMail")
+	public String
+	sendMail(@RequestBody EmailDetails details)
+	{
+		String status
+			= emailService.sendSimpleMail(details);
+
+		return status;
+	}
+
+	// Sending email with attachment
+	@PostMapping("/sendMailWithAttachment")
+	public String sendMailWithAttachment(
+		@RequestBody EmailDetails details)
+	{
+		String status
+			= emailService.sendMailWithAttachment(details);
+
+		return status;
+	}
+}
+
---
+++ backend_db/src/main/java/net/javaguides/springboot/security/services/EmailService.java
@@ -0,0 +1,20 @@
+package net.javaguides.springboot.security.services;
+
+// Java Program to Illustrate Creation Of
+// Service Interface
+
+
+// Importing required classes
+import net.javaguides.springboot.entity.EmailDetails;
+
+// Interface
+public interface EmailService {
+
+	// Method
+	// To send a simple email
+	String sendSimpleMail(EmailDetails details);
+
+	// Method
+	// To send an email with attachment
+	String sendMailWithAttachment(EmailDetails details);
+}
